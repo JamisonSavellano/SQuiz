cc30b564b06bc8bc67889d89cd346a879ae9c307
JamisonSavelleno <jamison2006@icloud.com>
2024-06-10 15:05:13 -0700

    added alot

--- webapp.py
+++ webapp.py
@@ -4,39 +4,93 @@ from markupsafe import Markup
 import os
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 @app.route("/")
 def render_main():
     return render_template('home.html')    
     
-@app.route("/p1")
+@app.route("/p1",methods=['GET','POST'])
 def render_page1():
+    session.clear()
+    print(session)
     return render_template('page1.html')
     
 @app.route("/p2",methods=['GET','POST'])
 def render_page2():
-    session["A1"]=request.form['temp']
-    return render_template('page2.html')
+    print(session)
+    if "A1" in session:
+        return render_template('page2.html')
+    else:
+        if request.method == 'POST':
+            session["A1"] = request.form['temp']
+        return render_template('page2.html')
     
-@app.route("/p3")
+@app.route("/p3",methods=['GET','POST'])
 def render_page3():
+    print(session)
+    if "A2" in session:
+        return render_template('page3.html')
+    else:
+        if request.method == 'POST':
+            session["A2"] = request.form['temp']
     return render_template('page3.html')
     
-@app.route("/p4")
+@app.route("/p4",methods=['GET','POST'])
 def render_page4():
+    print(session)
+    if "A3" in session:
+        return render_template('page4.html')
+    else:
+        if request.method == 'POST':
+            session["A3"] = request.form['temp']
     return render_template('page4.html')
     
-@app.route("/p5")
+@app.route("/p5",methods=['GET','POST'])
 def render_page5():
+    print(session)
+    if "A4" in session:
+        return render_template('page5.html')
+    else:
+        if request.method == 'POST':
+            session["A4"] = request.form['temp']
     return render_template('page5.html')
     
-@app.route("/p6")
-def render_page6():
-    return render_template('checkPage.html')
-    
-@app.route("/f")
+@app.route("/f",methods=['GET','POST'])
 def render_pageFinal():
-    return render_template('FinalPage.html')
+    x = 0
+    y = {}
+    if "A5" in session:
+        return render_template('FinalPage.html', numCorrect = x)
+    else:
+        if request.method == 'POST':
+            session["A5"] = request.form['temp']
+            if session["A1"] == "2":
+               x += 1 
+               y["Q1"] = "correct"
+            else:
+               y["Q1"] = "incorrect"
+            if session["A2"] == "3":
+               x += 1
+               y["Q2"] = "correct"
+            else:
+               y["Q2"] = "incorrect"
+            if session["A3"] == "10":
+               x += 1
+               y["Q3"] = "correct"
+            else:
+               y["Q3"] = "incorrect"
+            if session["A4"] == "5":
+               x += 1
+               y["Q4"] = "correct"
+            else:
+               y["Q4"] = "incorrect"
+            if session["A5"] == "256":
+               x += 1
+               y["Q5"] = "correct"
+            else:
+               y["Q5"] = "incorrect"
+            return render_template('FinalPage.html', numCorrect = x, qCorrect = y)
     
 if __name__=="__main__":
     app.run(debug=True)
